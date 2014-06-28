b7301a1c6a14513db0ad07782ba3622f96f93fea
Moritz Finsterwalder <moritz.finsterwalder@gmail.com>
2014-06-28 21:40:51 +0200

    tested lab15 hardware

---
+++ src/labs/Lab15_VirtualSpaceInvaders/spaceinvaders_tony_l.d
@@ -0,0 +1,7 @@
+.\spaceinvaders_tony_l.o: SpaceInvaders_Tony_L.c
+.\spaceinvaders_tony_l.o: ..//tm4c123gh6pm.h
+.\spaceinvaders_tony_l.o: Nokia5110.h
+.\spaceinvaders_tony_l.o: Random.h
+.\spaceinvaders_tony_l.o: TExaS.h
+.\spaceinvaders_tony_l.o: Subfile1.c
+.\spaceinvaders_tony_l.o: Subfile2.c
